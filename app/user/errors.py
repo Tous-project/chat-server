7895ea1dce818e5dbf304d2e3aa2e3223fabd508
jungmir <ham4051@gmail.com>
2022-11-10 23:46:57 +0900

    Feat: Add custom error class

---
+++ app/user/errors.py
@@ -0,0 +1,11 @@
+from common.errors import NotFoundError, DatabaseIntegrityError
+
+
+class UserNotFoundByIdError(NotFoundError):
+    def __init__(self, id: str) -> None:
+        super().__init__(f"User(id={id!r}) not found")
+
+
+class CannotCreateUserError(DatabaseIntegrityError):
+    def __init__(self, name: str, email: str) -> None:
+        super().__init__(f"Cannot create User({name=!r}, {email=!r})")
