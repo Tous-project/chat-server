b0ee70b39d43cbcb813974929b8d6bfb2636651b
jungmir <ham4051@gmail.com>
2022-11-12 21:42:55 +0900

    Feat: Add user session service

--- app/user/service.py
+++ app/user/service.py
@@ -3,8 +3,13 @@ from __future__ import annotations
 
 from typing import Iterator
 
-from user.models import User
-from user.repository import UserRepository
+from .errors import (
+    InvalidUserSessionError,
+    SessionAlreadyExistError,
+    UserNotLoggedInError,
+)
+from .models import User, UserSession
+from .repository import UserRepository, UserSessionRepository
 
 
 class UserService:
@@ -22,3 +27,32 @@ class UserService:
 
     def delete_by_id(self, id: int) -> None:
         return self._repository.delete(id=id)
+
+
+class UserSessionService:
+    def __init__(self, user_session_repository: UserSessionRepository):
+        self._repository = user_session_repository
+
+    def get_by_user_id(self, user_id: int) -> UserSession:
+        return self._repository.get_by_user_id(user_id=user_id)
+
+    def get_by_session_id(self, session_id: str) -> UserSession:
+        return self._repository.get_by_session_id(session_id=session_id)
+
+    def create(self, user_id: int) -> UserSession:
+        is_exist = self._repository.get_by_user_id(user_id=user_id)
+        if is_exist:
+            raise SessionAlreadyExistError(user_id)
+        return self._repository.create(user_id=user_id)
+
+    def delete_by_session_id(self, session_id: str) -> None:
+        is_exist = self._repository.get_by_session_id(session_id=session_id)
+        if not is_exist:
+            raise InvalidUserSessionError(session_id)
+        return self._repository.delete_by_session_id(session_id=session_id)
+
+    def delete_all(self, user_id: int) -> None:
+        is_exist = self._repository.get_all(user_id=user_id)
+        if not is_exist:
+            raise UserNotLoggedInError(user_id=user_id)
+        return self._repository.delete_all(user_id=user_id)
