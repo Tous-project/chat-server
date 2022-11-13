62c1cf8e283ac101b2754f435e18f46f8462aa55
jungmir <ham4051@gmail.com>
2022-11-13 22:46:45 +0900

    Feat: Remove unused variable

--- app/common/container.py
+++ app/common/container.py
@@ -57,8 +57,6 @@ class ServiceContainer(containers.DeclarativeContainer):
 
 
 class ApplicationContainer(containers.DeclarativeContainer):
-    cfg = config("db")
-
     db = providers.Container(DatabaseContainer)
 
     repository = providers.Container(RepositoryContainer, db=db)
