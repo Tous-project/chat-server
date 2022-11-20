acd846e22eb0af6423a00bb83def2e54064a603e
jungmir <ham4051@gmail.com>
2022-11-20 12:33:47 +0900

    feat: Add authenticate by session id

--- app/chat/router.py
+++ app/chat/router.py
@@ -7,13 +7,24 @@ from chat.socket_handler import SocketHandler
 from common.container import ApplicationContainer
 from common.response import ErrorResponse
 from dependency_injector.wiring import Provide, inject
-from fastapi import APIRouter, Depends, Response, WebSocket, WebSocketDisconnect, status
+from fastapi import (
+    APIRouter,
+    Depends,
+    Header,
+    Response,
+    WebSocket,
+    WebSocketDisconnect,
+    status,
+)
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 from user.service import UserService
 
 from .request import CreateChatRoom
 from .service import ChatRoomMemberService, ChatRoomService
+
+from common.authenticate import Session  # isort:skip
+
 
 router = APIRouter()
 CHAT_ROOM_TAGS = "Chat Room"
@@ -30,6 +41,8 @@ async def healthz() -> JSONResponse:
 @inject
 async def get_all_chat_rooms(
     chat_room: ChatRoomService = Depends(Provide[ApplicationContainer.service.chat_room]),
+    x_session_id: str = Header(...),
+    current_user: Session = Depends(Session.verify),
 ) -> JSONResponse:
     all_chat_rooms = chat_room.get_all()
     return JSONResponse(jsonable_encoder(all_chat_rooms), status_code=status.HTTP_200_OK)
@@ -40,6 +53,8 @@ async def get_all_chat_rooms(
 async def create_chat_room(
     create_chat_room: CreateChatRoom,
     chat_room: ChatRoomService = Depends(Provide[ApplicationContainer.service.chat_room]),
+    x_session_id: str = Header(...),
+    current_user: Session = Depends(Session.verify),
 ) -> JSONResponse:
     try:
         new_chat_room = chat_room.create(**create_chat_room.dict())
@@ -61,6 +76,8 @@ async def create_chat_room(
 async def delete_chat_room_by_id(
     room_id: int,
     chat_room: ChatRoomService = Depends(Provide[ApplicationContainer.service.chat_room]),
+    x_session_id: str = Header(...),
+    current_user: Session = Depends(Session.verify),
 ) -> Union[JSONResponse, Response]:
     try:
         chat_room.delete_by_id(room_id=room_id)
@@ -79,13 +96,16 @@ async def delete_chat_room_by_id(
 @inject
 async def join_chat_room(
     room_id: int,
-    user_id: int,
     chat_room_member: ChatRoomMemberService = Depends(
         Provide[ApplicationContainer.service.chat_room_member]
     ),
+    x_session_id: str = Header(...),
+    current_user: Session = Depends(Session.verify),
 ) -> JSONResponse:
     try:
-        new_chat_room_member = chat_room_member.join(room_id=room_id, user_id=user_id)
+        new_chat_room_member = chat_room_member.join(
+            room_id=room_id, user_id=current_user.id
+        )
     except Exception as exception:
         error = ErrorResponse(
             error_message="Can't join the chat room",
@@ -107,6 +127,8 @@ async def get_all_chat_room_members(
         Provide[ApplicationContainer.service.chat_room_member]
     ),
     user: UserService = Depends(Provide[ApplicationContainer.service.user]),
+    x_session_id: str = Header(...),
+    current_user: Session = Depends(Session.verify),
 ) -> JSONResponse:
     try:
         all_chat_room_members = chat_room_member.get_all_joined_members(room_id=room_id)
@@ -123,17 +145,18 @@ async def get_all_chat_room_members(
     return JSONResponse(jsonable_encoder(users), status_code=status.HTTP_200_OK)
 
 
-@router.delete("/rooms/{room_id}/members/{user_id}", tags=[CHAT_ROOM_MEMBER_TAGS])
+@router.delete("/rooms/{room_id}/members", tags=[CHAT_ROOM_MEMBER_TAGS])
 @inject
 async def leave_chat_room(
     room_id: int,
-    user_id: int,
     chat_room_member: ChatRoomMemberService = Depends(
         Provide[ApplicationContainer.service.chat_room_member]
     ),
+    x_session_id: str = Header(...),
+    current_user: Session = Depends(Session.verify),
 ) -> Union[Response, JSONResponse]:
     try:
-        chat_room_member.leave(room_id=room_id, user_id=user_id)
+        chat_room_member.leave(room_id=room_id, user_id=current_user.id)
     except Exception as exception:
         error = ErrorResponse(
             error_message="Can't leave chat room",
@@ -145,16 +168,19 @@ async def leave_chat_room(
     return Response(status_code=status.HTTP_200_OK)
 
 
-@router.get("/members/{user_id}/rooms", tags=[CHAT_ROOM_MEMBER_TAGS])
+@router.get("/rooms/me", tags=[CHAT_ROOM_MEMBER_TAGS])
 @inject
-async def get_all_chat_rooms_the_user_joined(
-    user_id: int,
+async def get_all_chat_rooms_i_joined(
     chat_room_member: ChatRoomMemberService = Depends(
         Provide[ApplicationContainer.service.chat_room_member]
     ),
+    x_session_id: str = Header(...),
+    current_user: Session = Depends(Session.verify),
 ) -> JSONResponse:
     try:
-        joind_all_chat_rooms = chat_room_member.get_all_joined_chat_rooms(user_id=user_id)
+        joind_all_chat_rooms = chat_room_member.get_all_joined_chat_rooms(
+            user_id=current_user.id
+        )
     except Exception as exception:
         error = ErrorResponse(
             error_message="Can't inquire chat room the user joined",
@@ -169,11 +195,16 @@ async def get_all_chat_rooms_the_user_joined(
 
 
 @router.websocket("/ws/rooms/{room_id}")
-async def enter_chatting_room(socket: WebSocket, room_id: str, username: str):
+async def enter_chatting_room(
+    socket: WebSocket,
+    room_id: str,
+    x_session_id: str = Header(...),
+    current_user: Session = Depends(Session.verify),
+) -> None:
     if room_id not in chatting_rooms:
         chatting_rooms[room_id] = ChattingRoom()
     room = chatting_rooms[room_id]
-    user = SocketHandler(socket, username)
+    user = SocketHandler(socket, current_user.name)
     await room.enter(user)
     try:
         while True:
