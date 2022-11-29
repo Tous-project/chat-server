0501d5f4ceb0e9fc58c6abfda208a61f45dfab1e
jungmir <ham4051@gmail.com>
2022-11-30 00:05:22 +0900

    feat: Add email validation using regex

--- app/user/request.py
+++ app/user/request.py
@@ -1,13 +1,22 @@
 # -*- coding: utf-8 -*-
 from __future__ import annotations
 
-from pydantic import BaseModel
+import re
+
+from pydantic import BaseModel, validator
 
 
 class CreateUser(BaseModel):
     email: str
     password: str
     name: str
+
+    @validator("email")
+    def email_validator(cls, value: str) -> str:
+        email_regex = r"^[\w\-\.]+@([\w-]+\.)+[\w-]{2,4}$"
+        if re.match(email_regex, value):
+            return value
+        raise ValueError(f"email={value!r} is invalid format")
 
 
 class UpdateUser(BaseModel):
